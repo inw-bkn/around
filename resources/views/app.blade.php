7629005853949c6ad0715cdac951edba82a4ecc4
Koramit Pichanaharee <koramit.p@gmail.com>
2021-01-22 20:43:52 +0700

    add italic 200

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@200;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,200;0,700;1,200&display=swap"
+        rel="stylesheet">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>

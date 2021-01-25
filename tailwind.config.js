6c9967dce8a7da7dcafdba0946edea87fa812a00
Koramit Pichanaharee <koramit.p@gmail.com>
2021-01-25 18:09:33 +0700

    add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'soft-theme-light': '#FDF6E3',
+                'bitter-theme-light': '#ff0',
+                'thick-theme-light': '#586E75',
+                'dark-theme-light': '#465C62',
+            }
+        },
     },
     variants: {
         extend: {

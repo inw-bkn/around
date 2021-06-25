028cfa5a6f7c0a7c55b403f9cc5a17e02123fc42
Koramit Pichanaharee <koramit.p@gmail.com>
2021-06-25 22:11:16 +0700

    setup front build

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,11 +17,11 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
-    <link
-        href="https://fonts.googleapis.com/css2?family=Lobster&family=Sarabun:ital,wght@0,200;0,600;1,200;1,600&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Sarabun:ital,wght@0,200;0,600;1,200;1,600&display=swap" rel="stylesheet">
+
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    @routes
 </head>
 
 <body class="m-0 font-sarabun font-extralight text-gray-700 bg-soft-theme-light">
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,27 +1,24 @@
 const mix = require('laravel-mix');
 const path = require('path');
 
-mix.js('resources/js/app.js', 'public/js').vue({ version: 3 })
+mix.js('resources/js/app.js', 'public/js')
+    .vue({ version: 3, runtimeOnly: true })
+    .alias({ '@': 'resources/js' })
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
-    ]);
-
-mix.alias({
-    '@': path.join(__dirname, 'resources/js'),
-});
-
-mix.webpackConfig(webpack => {
-    return {
-        plugins: [
-            new webpack.DefinePlugin({
-                '__VUE_OPTIONS_API__': true,
-                '__VUE_PROD_DEVTOOLS__': false
-            })
-        ],
-        output: {
-            chunkFilename: 'js/[name].js?id=[chunkhash]'
-        }
-    };
-})
+    ])
+    .webpackConfig(webpack => {
+        return {
+            plugins: [
+                new webpack.DefinePlugin({
+                    '__VUE_OPTIONS_API__': true,
+                    '__VUE_PROD_DEVTOOLS__': false
+                })
+            ],
+            output: { // use split code
+                chunkFilename: 'js/[name].js?id=[chunkhash]',
+            }
+        };
+    })
     .sourceMaps(false)
     .version();

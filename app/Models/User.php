bd6b4920bb8b7b742569a2b8e7d3eb52f271f129
Koramit Pichanaharee <koramit.p@gmail.com>
2021-06-28 20:51:35 +0700

    setup user role abilitie

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Resources\Division;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Cache;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -59,5 +61,69 @@ class User extends Authenticatable
             fn ($query) => $query->where('name', 'ilike', $name),
             fn ($query) => $query->whereName($name)
         );
+    }
+
+    /**
+     * A user may be assigned many roles.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class)->withTimestamps();
+    }
+
+    /**
+     * Assign a new role to the user.
+     *
+     * @param  mixed  $role
+     */
+    public function assignRole($role)
+    {
+        if (is_string($role)) {
+            $role = Role::whereName($role)->firstOrCreate(['name' => $role]);
+        }
+
+        $this->roles()->syncWithoutDetaching($role);
+
+        unset($this->roles); // reload for new role
+        Cache::put("uid-{$this->id}-abilities", $this->roles->map->abilities->flatten()->pluck('name')->unique(), config('session.lifetime') * 60);
+        Cache::put("uid-{$this->id}-role-names", $this->roles->pluck('name'), config('session.lifetime') * 60);
+    }
+
+    public function getTimezoneAttribute()
+    {
+        return 'asia/bangkok';
+    }
+
+    public function getAbilitiesAttribute()
+    {
+        return Cache::remember("uid-{$this->id}-abilities", config('session.lifetime') * 60, function () {
+            unset($this->roles); // reload for new role
+
+            // if unique() is not activated then the output is an array
+            // but the output is an associated array so, provide
+            // flatten() to garantee output awlays an array
+            return $this->roles->map->abilities->flatten()->pluck('name')->unique()->flatten();
+        });
+    }
+
+    public function getRoleNamesAttribute()
+    {
+        return Cache::remember("uid-{$this->id}-role-names", config('session.lifetime') * 60, function () {
+            unset($this->roles);
+
+            return $this->roles->pluck('name');
+        });
+    }
+
+    public function getFullNameAttribute()
+    {
+        return $this->profile['full_name'];
+    }
+
+    public function getTelNoAttribute()
+    {
+        return $this->profile['tel_no'];
     }
 }

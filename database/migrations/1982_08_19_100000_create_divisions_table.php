1d273c0329ff1de27e7ae13d4fa36c223c89f990
Koramit Pichanaharee <koramit.p@gmail.com>
2021-07-03 22:04:13 +0700

    refactor seeder

--- database/migrations/1982_08_19_100000_create_divisions_table.php
+++ database/migrations/1982_08_19_100000_create_divisions_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Division;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -24,43 +23,6 @@ class CreateDivisionsTable extends Migration
             $table->unsignedInteger('creator_id')->index()->nullable();
             $table->timestamps();
         });
-
-        $datetime = now();
-
-        Division::insert([
-            [
-                'name' => 'คณะแพทยศาสตร์ศิริราชพยาบาล',
-                'name_eng' => 'Faculty of Medicine Siriraj Hospital',
-                'name_eng_short' => 'faculty',
-                'department' => 'faculty',
-                'created_at' => $datetime,
-                'updated_at' => $datetime,
-            ],
-            [
-                'name' => 'ภาควิชาอายุรศาสตร์',
-                'name_eng' => 'Department of Medicine',
-                'name_eng_short' => 'medicine',
-                'department' => 'medicine',
-                'created_at' => $datetime,
-                'updated_at' => $datetime,
-            ],
-            [
-                'name' => 'ฝ่ายเภสัชกรรม',
-                'name_eng' => 'Pharmacy Department',
-                'name_eng_short' => 'pharmacy',
-                'department' => 'pharmacy',
-                'created_at' => $datetime,
-                'updated_at' => $datetime,
-            ],
-            [
-                'name' => 'วักกะวิทยา',
-                'name_eng' => 'Division of Nephrology',
-                'name_eng_short' => 'nephrology',
-                'department' => 'medicine',
-                'created_at' => $datetime,
-                'updated_at' => $datetime,
-            ],
-        ]);
     }
 
     /**
